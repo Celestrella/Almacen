7347db675703c203b6ddd3f133ccce0eb98a64da
Celestrella <43602639+Celestrella@users.noreply.github.com>
2019-10-28 12:57:50 +0100

    Add files via upload

---
+++ Almacen/AlmacenFPBasicaCNA/AlmacenCNA/Entidades/Empresa.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Empresa
+    {
+
+        public String NombreEmpresa { get; set; }
+        public String Logo { get; set; }
+
+
+
+        public Empresa(string nombreEmpresa, string logo)
+        {
+            NombreEmpresa = nombreEmpresa;
+            Logo = logo;
+           
+        }
+
+        public Empresa()
+        {
+        }
+
+        public Empresa(string nombreEmpresa)
+        {
+            NombreEmpresa = nombreEmpresa;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Empresa empresa &&
+                   NombreEmpresa.ToUpper() == empresa.NombreEmpresa.ToUpper();
+        }
+    }
+}
